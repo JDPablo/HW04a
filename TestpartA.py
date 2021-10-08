1ff3e43e6212d9432135cc65b15ca280507b7466
JD Pablo <jdpablo1002@gmail.com>
2021-10-07 21:30:51 -0400

    refactored code; added try except blocks

--- TestpartA.py
+++ TestpartA.py
@@ -12,9 +12,6 @@ class TestGetRepos(unittest.TestCase):
     def testJoan(self): 
         self.assertIsNotNone(getRepos('joantubungbanua'))
 
-    def testKnownInvalid(self): 
-        self.assertIsNone(getRepos('invalidacccccc'))
-
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
--- partA.py
+++ partA.py
@@ -2,28 +2,32 @@ import json
 import requests
 
 def getRepos(githubID):
-    reposAPILink = "https://api.github.com/users/"
-    commitsAPILink = "https://api.github.com/repos/"
-    repos = []
     output = []
-    reposURL = reposAPILink + f'{githubID}/repos'
-    reposURL = requests.get(url = reposURL)
-    reposURL = json.loads(reposURL.text)
+    githubIDURL = 'https://api.github.com/users/{}/repos'.format(githubID)
+    githubInfo = requests.get(githubIDURL)
+    reposInfo = json.loads(githubInfo.text)
+    output.append('User: {}'.format(githubID))
 
-    for repo1 in reposURL:
-        repos.append(repo1['name'])
+    try:
+        reposInfo[0]['name']
+    except (TypeError, KeyError, IndexError):
+        return 'error encountered while getting repos'
 
-    for repo2 in repos:
-        commitsURL = commitsAPILink + f'{githubID}/{repo2}/commits'
-        jsonFile = json.loads((requests.get(url = commitsURL)).text)
-        output.append(f'Repo: {repo2}\nCommits: {len(jsonFile)}\n')
-
+    try:
+        for repo in reposInfo:
+            repoName = repo['name']
+            repoURL = 'https://api.github.com/repos/{}/{}/commits'.format(githubID, repoName)
+            repoInfo = requests.get(repoURL)
+            repoData = json.loads(repoInfo.text)
+            output.append('Repo: {}\nCommits: {}'.format(repoName, len(repoData)))
+    except (TypeError, KeyError, IndexError):
+        return 'error encountered while getting commits'
     return output
 
 def main():
     idInput = input("Please enter the Github ID of the account you'd like to see the repos of: ")
-    for repo in getRepos(idInput):
-        print(repo)
+    for userInput in getRepos(idInput):
+        print(userInput)
 
 if __name__ == '__main__':
     main()
