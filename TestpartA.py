01b4f882a5cc7f16ad4b4d14241ac96486356d0d
JD Pablo <jdpablo1002@gmail.com>
2021-10-09 19:52:32 -0400

    mocked all api calls

--- TestpartA.py
+++ TestpartA.py
@@ -1,16 +1,32 @@
 import unittest
+from unittest.mock import patch
 
+import partA
 from partA import getRepos
 
+githubID1 = 'JDPablo'
+githubID2 = 'BlackRoseRipp'
+githubID3 = 'joantubungbanua'
+repo = 'hw04a'
+
 class TestGetRepos(unittest.TestCase):
-    def testJD(self): 
-        self.assertIsNotNone(getRepos('JDPablo'))
+    @patch('partA.getRepos')
+    def testJD(self, mock_getRepos):
+        mock_getRepos.returnValue = [repo]
+        repos = partA.getRepos('JDPablo')
+        self.assertIsNotNone(repos, list)
 
-    def testTom(self): 
-        self.assertIsNotNone(getRepos('BlackRoseRipp'))
+    @patch('partA.getRepos')
+    def testTom(self, mock_getRepos):
+        mock_getRepos.returnValue = [repo]
+        repos = partA.getRepos('BlackRoseRipp')
+        self.assertIsNotNone(repos, list)
 
-    def testJoan(self): 
-        self.assertIsNotNone(getRepos('joantubungbanua'))
+    @patch('partA.getRepos')
+    def testJoan(self, mock_getRepos):
+        mock_getRepos.returnValue = [repo]
+        repos = partA.getRepos('joantubungbanua')
+        self.assertIsNotNone(repos, list)
 
 if __name__ == '__main__':
     print('Running unit tests')
